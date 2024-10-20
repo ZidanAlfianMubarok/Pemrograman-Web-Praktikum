43329157372ae27cfde4628afd86270569373b81
kangzid <zidanalmubarok652@gmail.com>
2024-10-20 09:28:55 +0700

    four

--- pytoko/app/models.py
+++ pytoko/app/models.py
@@ -23,3 +23,34 @@ class Todos(db.Model):
 
     def __repr__(self):
         return '<Todo {}>'.format(self.todo)
+
+# Class untuk tabel CategoryProduct
+class CategoryProduct(db.Model):
+    __tablename__ = 'category_product'
+    id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # Relasi satu ke banyak dengan Product
+    products = db.relationship('Product', backref='category', lazy=True)
+
+    def __repr__(self):
+        return f"<CategoryProduct {self.name}>"
+
+# Class untuk tabel Product
+class Product(db.Model):
+    __tablename__ = 'product'
+    id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # Foreign key untuk relasi dengan CategoryProduct
+    category_id = db.Column(db.BigInteger, db.ForeignKey('category_product.id'), nullable=False)
+
+    def __repr__(self):
+        return f"<Product {self.name}>"
