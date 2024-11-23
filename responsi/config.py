41ff8a4ada4c44527873bfc167cf93c22c0032cd
kangzid <zidanalmubarok652@gmail.com>
2024-11-23 19:07:38 +0700

    add rest api

--- responsi/config.py
+++ responsi/config.py
@@ -5,6 +5,6 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="",  # Ganti dengan password MySQL Anda
+        password="sidakaton",  # Ganti dengan password MySQL Anda
         database="app_penjualan"
     )
--- restapi/app/routes.py
+++ restapi/app/routes.py
@@ -1,8 +1,53 @@
-from flask_restful import Resource
+from flask import Flask, jsonify
+from flask_restful import Api, Resource
 
-def initialize_routes(api):
-    api.add_resource(HelloWorld, '/')
+app = Flask(__name__)
+api = Api(app)
 
 class HelloWorld(Resource):
     def get(self):
-        return {'hello': 'world'}
+        """
+        Get a greeting message.
+        ---
+        responses:
+          200:
+            description: A greeting message.
+            schema:
+              type: object
+              properties:
+                test:
+                  type: string
+                  example: "hello world"
+        """
+        return jsonify({'test': 'hallo world'})
+
+class NotFound(Resource):
+    def get(self, resource):
+        """
+        Handle not found resource.
+        ---
+        parameters:
+          - name: resource
+            in: path
+            type: string
+            required: true
+            description: The resource that was not found
+        responses:
+          404:
+            description: Resource not found
+            schema:
+              type: object
+              properties:
+                message:
+                  type: string
+                  example: "Resource not found"
+        """
+        return {'message': 'Resource not found'}, 404
+
+def initialize_routes(api):
+    api.add_resource(HelloWorld, '/')
+    api.add_resource(NotFound, '/<path:resource>')
+
+if __name__ == '__main__':
+    initialize_routes(api)
+    app.run(debug=True)
